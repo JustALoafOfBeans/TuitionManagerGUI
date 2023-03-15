1eeee443f6874f963b7a40226d136a7836ab54a0
JustALoafOfBeans <victoriachen4520@gmail.com>
2023-03-15 00:52:16 -0400

    init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tuitionmanagergui {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.tuitionmanagergui to javafx.fxml;
+    exports com.example.tuitionmanagergui;
+}
