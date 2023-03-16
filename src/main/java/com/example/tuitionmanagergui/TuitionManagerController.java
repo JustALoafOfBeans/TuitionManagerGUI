e6d210629593950ec513944b131746c15fd9cfe3
JustALoafOfBeans <victoriachen4520@gmail.com>
2023-03-16 02:31:25 -0400

    refactor file names

--- src/main/java/com/example/tuitionmanagergui/TuitionManagerController.java
+++ src/main/java/com/example/tuitionmanagergui/TuitionManagerController.java
@@ -3,7 +3,7 @@ package com.example.tuitionmanagergui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class TuitionManagerController {
     @FXML
     private Label welcomeText;
 
