a1b2a20e959f5e95ebd8c576d509bb5cfc1c8a2b
brizbird14 <brizbird14@gmail.com>
2023-03-17 11:12:34 -0400

    Disabling buttons

--- src/main/java/com/example/tuitionmanagergui/TuitionManagerController.java
+++ src/main/java/com/example/tuitionmanagergui/TuitionManagerController.java
@@ -9,13 +9,48 @@ public class TuitionManagerController {
     @FXML
     public DatePicker DobInput;
     public RadioButton MajorBAIT, MajorCS, MajorECE, MajorITI, MajorMATH;
-    public RadioButton ResidentButton;
+    public RadioButton ResidentButton, NonResidentButton, TristateButton, TriNYButton, TriCTButton, IntlButton;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void initialize() {
+        System.out.println("Initial things"); // todo remove lol
+        TristateButton.setDisable(true);
+        TriNYButton.setDisable(true);
+        TriCTButton.setDisable(true);
+        IntlButton.setDisable(true);
+    }
+
+    @FXML
+    protected void onResidentTrue() { // Disables related Non-Resident button options
+        TristateButton.setDisable(true);
+        TriNYButton.setDisable(true);
+        TriCTButton.setDisable(true);
+        IntlButton.setDisable(true);
+    }
+
+    @FXML
+    protected void onNonresidentTrue() { // Enables Non-REsident button options
+        TristateButton.setDisable(false);
+        IntlButton.setDisable(false);
+    }
+
+    @FXML
+    protected void onTristateTrue() { // Enables Tristate button options
+        TriNYButton.setDisable(false);
+        TriCTButton.setDisable(false);
+    }
+
+    @FXML
+    protected void onIntlTrue() { // Disables Tristate Non-res button options
+        TriNYButton.setDisable(true);
+        TriCTButton.setDisable(true);
     }
 
     @FXML
